48e6f1fa7659bed5110c1073c9eb556d08a8c7c5
Kasun <kasun.v@synaxiom.com>
2026-03-23 16:23:01 +0530

    Create Core ValueObjects

---
+++ app/Modules/Core/Domain/ValueObjects/Metadata.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Core\Domain\ValueObjects;
+
+class Metadata extends ValueObject
+{
+    private array $data;
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function get(string $key, $default = null)
+    {
+        return $this->data[$key] ?? $default;
+    }
+
+    public function toArray(): array
+    {
+        return $this->data;
+    }
+
+    public static function fromArray(array $data): static
+    {
+        return new static($data);
+    }
+}
---
+++ app/Modules/Core/Domain/ValueObjects/ValidatedEmail.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Core\Domain\ValueObjects;
+
+class ValidatedEmail extends Email
+{
+    public function __construct(string $value)
+    {
+        parent::__construct($value);
+        // add custom validation, e.g., check domain
+        if (!str_ends_with($value, '@company.com')) {
+            throw new \InvalidArgumentException('Only company emails allowed');
+        }
+    }
+}
