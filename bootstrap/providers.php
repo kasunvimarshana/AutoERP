ce97528de177e89797a1ef07b407f21e731576f9
Kasun <kasun.v@synaxiom.com>
2026-03-28 11:38:04 +0530

    Implement Customer and Supplier Modules

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,8 @@
 use App\Providers\AppServiceProvider;
 use Modules\Auth\Infrastructure\Providers\AuthModuleServiceProvider;
 use Modules\Account\Infrastructure\Providers\AccountServiceProvider;
+use Modules\Customer\Infrastructure\Providers\CustomerServiceProvider;
+use Modules\Supplier\Infrastructure\Providers\SupplierServiceProvider;
 use Modules\Brand\Infrastructure\Providers\BrandServiceProvider;
 use Modules\Category\Infrastructure\Providers\CategoryServiceProvider;
 use Modules\Core\Infrastructure\Providers\CoreServiceProvider;
@@ -24,4 +26,6 @@ return [
     BrandServiceProvider::class,
     CategoryServiceProvider::class,
     AccountServiceProvider::class,
+    SupplierServiceProvider::class,
+    CustomerServiceProvider::class,
 ];
