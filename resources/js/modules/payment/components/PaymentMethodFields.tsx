f5406cc9a42da06eb8aa31766b31c29809856d36
kasun vimarshana <kasunvmail@gmail.com>
2026-06-30 03:55:38 +0530

    Remove ignored Payment branch input

--- resources/js/modules/payment/components/PaymentMethodFields.tsx
+++ resources/js/modules/payment/components/PaymentMethodFields.tsx
@@ -15,13 +15,11 @@ export function PaymentMethodFields({ kind, metadata, onChange }: PaymentMethodF
                 <Input label="Cheque number" value={metadata.cheque_number ?? ''} onChange={(event) => onChange('cheque_number', event.target.value)} />
                 <Input label="Cheque date" type="date" value={metadata.cheque_date ?? ''} onChange={(event) => onChange('cheque_date', event.target.value)} />
                 <Input label="External bank" value={metadata.bank_account ?? ''} onChange={(event) => onChange('bank_account', event.target.value)} />
-                <Input label="External branch" value={metadata.bank_branch ?? ''} onChange={(event) => onChange('bank_branch', event.target.value)} />
             </>}
             {kind === 'bank_transfer' && <>
                 <Input label="Transfer reference" value={metadata.transfer_reference ?? ''} onChange={(event) => onChange('transfer_reference', event.target.value)} />
                 <Input label="Transfer date" type="date" value={metadata.transfer_date ?? ''} onChange={(event) => onChange('transfer_date', event.target.value)} />
                 <Input label="External bank" value={metadata.bank_account ?? ''} onChange={(event) => onChange('bank_account', event.target.value)} />
-                <Input label="External branch" value={metadata.bank_branch ?? ''} onChange={(event) => onChange('bank_branch', event.target.value)} />
             </>}
             {kind === 'card' && <>
                 <Input label="Terminal or acquirer" value={metadata.terminal ?? ''} onChange={(event) => onChange('terminal', event.target.value)} />
