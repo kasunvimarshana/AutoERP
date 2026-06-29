b3b89ff2e5cf977f246d7b39ef95188f96468d40
kasun vimarshana <kasunvmail@gmail.com>
2026-06-29 18:23:54 +0530

    refactor(payment): model refund transaction details

--- app/Modules/Payment/DTOs/PaymentRefundData.php
+++ app/Modules/Payment/DTOs/PaymentRefundData.php
@@ -12,6 +12,11 @@ final readonly class PaymentRefundData
         public string $refundDate,
         public string $amount,
         public ?int $paymentMethodId = null,
+        public ?string $referenceNumber = null,
+        public ?string $externalBankName = null,
+        public ?string $externalBankBranch = null,
+        public ?string $instrumentNumber = null,
+        public ?string $instrumentDate = null,
         public ?string $reason = null,
         public ?int $refundedBy = null,
     ) {}
