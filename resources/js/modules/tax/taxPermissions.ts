8e9117a1c05c5db80f234798bb9771e970206686
kasun vimarshana <kasunvmail@gmail.com>
2026-07-09 07:46:26 +0530

    Protect tax routes with permissions

---
+++ resources/js/modules/tax/taxPermissions.ts
@@ -0,0 +1,13 @@
+export const taxPermissions = {
+    lookupsView: 'tax.lookups.view',
+    calculationsRun: 'tax.calculations.run',
+    taxesView: 'tax.taxes.view',
+    taxesManage: 'tax.taxes.manage',
+    groupsView: 'tax.groups.view',
+    groupsManage: 'tax.groups.manage',
+    profilesView: 'tax.profiles.view',
+    profilesManage: 'tax.profiles.manage',
+    postingProfilesView: 'tax.posting_profiles.view',
+    postingProfilesManage: 'tax.posting_profiles.manage',
+    reportsView: 'tax.reports.view',
+} as const;
