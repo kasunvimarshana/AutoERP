c66691a29fa2cbdd9cf4e60cf54e264d82cfaf97
kasun vimarshana <kasunvmail@gmail.com>
2026-07-10 04:53:51 +0530

    Remove legacy finance posting line constructor surface

--- app/Modules/Finance/DTOs/FinancePostingLine.php
+++ app/Modules/Finance/DTOs/FinancePostingLine.php
@@ -6,32 +6,4 @@ namespace Modules\Finance\DTOs;
 
 final readonly class FinancePostingLine extends PostingLine
 {
-    /**
-     * @param  array<string, string|null>  $dimensions
-     */
-    public function __construct(
-        ?string $accountCode = null,
-        ?string $accountName = null,
-        string $debit = '0.000000',
-        string $credit = '0.000000',
-        ?string $description = null,
-        ?string $profileKey = null,
-        ?string $dimensionCode = null,
-        ?string $sourceLineType = null,
-        ?int $sourceLineId = null,
-        array $dimensions = [],
-    ) {
-        parent::__construct(
-            accountName: $accountName,
-            lineName: null,
-            debit: $debit,
-            credit: $credit,
-            description: $description,
-            profileKey: $profileKey,
-            dimensionCode: $dimensionCode,
-            sourceLineType: $sourceLineType,
-            sourceLineId: $sourceLineId,
-            dimensions: $dimensions,
-        );
-    }
 }
