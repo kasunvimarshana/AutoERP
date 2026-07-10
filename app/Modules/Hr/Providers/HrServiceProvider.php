95b5c7a75189c8189a07eaf2ae8543f4c7953070
kasun vimarshana <kasunvmail@gmail.com>
2026-07-10 07:46:39 +0530

    Register HR permission definitions

--- app/Modules/Hr/Providers/HrServiceProvider.php
+++ app/Modules/Hr/Providers/HrServiceProvider.php
@@ -1,8 +1,21 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Modules\Hr\Providers;
+
 use Illuminate\Support\ServiceProvider;
+use Modules\Core\Contracts\PermissionDefinitionRegistryInterface;
+use Modules\Hr\Services\HrAuthorizationService;
+
 final class HrServiceProvider extends ServiceProvider
 {
-    public function boot(): void { $this->loadRoutesFrom(__DIR__.'/../Routes/api.php'); $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations'); }
+    public function boot(): void
+    {
+        $this->app->make(PermissionDefinitionRegistryInterface::class)
+            ->register('hr', HrAuthorizationService::descriptions());
+
+        $this->loadRoutesFrom(__DIR__.'/../Routes/api.php');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
+    }
 }
