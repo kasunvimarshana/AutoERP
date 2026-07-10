aaaad84e8de538b79c0fff29dad789da1e00c644
kasun vimarshana <kasunvmail@gmail.com>
2026-07-10 08:40:37 +0530

    Require row version in HR status request

--- app/Modules/Hr/Http/Requests/ChangeEmployeeStatusRequest.php
+++ app/Modules/Hr/Http/Requests/ChangeEmployeeStatusRequest.php
@@ -1,12 +1,34 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Modules\Hr\Http\Requests;
+
 use Illuminate\Validation\Rule;
 use Modules\Core\Http\Requests\TenantScopedRequest;
 use Modules\Hr\DTOs\EmployeeStatusChangeData;
 use Modules\Hr\Enums\EmployeeStatus;
+
 final class ChangeEmployeeStatusRequest extends TenantScopedRequest
 {
-    public function rules(): array { return ['tenant_id' => ['required', 'integer', 'min:1'], 'organization_unit_id' => ['nullable', 'integer', 'min:1'], 'status' => ['required', Rule::enum(EmployeeStatus::class)], 'reason' => ['nullable', 'string']]; }
-    public function toData(): EmployeeStatusChangeData { return new EmployeeStatusChangeData(EmployeeStatus::from((string) $this->input('status')), $this->filled('reason') ? (string) $this->input('reason') : null, $this->currentUserId()); }
+    public function rules(): array
+    {
+        return [
+            'tenant_id' => ['required', 'integer', 'min:1'],
+            'organization_unit_id' => ['nullable', 'integer', 'min:1'],
+            'row_version' => ['required', 'integer', 'min:1'],
+            'status' => ['required', Rule::enum(EmployeeStatus::class)],
+            'reason' => ['nullable', 'string'],
+        ];
+    }
+
+    public function toData(): EmployeeStatusChangeData
+    {
+        return new EmployeeStatusChangeData(
+            EmployeeStatus::from((string) $this->input('status')),
+            (int) $this->input('row_version'),
+            $this->filled('reason') ? (string) $this->input('reason') : null,
+            $this->currentUserId(),
+        );
+    }
 }
