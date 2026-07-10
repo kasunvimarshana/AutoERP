cc1ad696460aaeb6ecdd419e17ed59ad38c82dd8
kasun vimarshana <kasunvmail@gmail.com>
2026-07-10 08:40:25 +0530

    Require row version for HR status changes

--- app/Modules/Hr/DTOs/EmployeeStatusChangeData.php
+++ app/Modules/Hr/DTOs/EmployeeStatusChangeData.php
@@ -1,5 +1,17 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Modules\Hr\DTOs;
+
 use Modules\Hr\Enums\EmployeeStatus;
-final readonly class EmployeeStatusChangeData { public function __construct(public EmployeeStatus $newStatus, public ?string $reason = null, public ?int $changedBy = null) {} }
+
+final readonly class EmployeeStatusChangeData
+{
+    public function __construct(
+        public EmployeeStatus $newStatus,
+        public int $rowVersion,
+        public ?string $reason = null,
+        public ?int $changedBy = null,
+    ) {}
+}
