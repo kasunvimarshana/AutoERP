f96d29cd7a659b1d3b27ee5b004cdb43481cc529
kasun vimarshana <kasunvmail@gmail.com>
2026-07-12 21:39:41 +0530

    feat(invoice): define finance source identity

---
+++ app/Modules/Invoice/Constants/InvoiceFinanceSource.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Invoice\Constants;
+
+final class InvoiceFinanceSource
+{
+    public const MODULE = 'invoice';
+
+    public const POSTING_TYPE = 'invoice';
+
+    public const REVERSAL_TYPE = 'invoice_reversal';
+
+    public const REVERSAL_LINE_TYPE = 'invoice_reversal_line';
+}
