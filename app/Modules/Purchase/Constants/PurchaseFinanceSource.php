db889b2b1ed64bb2e71cc601d322ff66733ea14e
kasun vimarshana <kasunvmail@gmail.com>
2026-07-12 22:04:39 +0530

    feat(purchase): define Finance source identities

---
+++ app/Modules/Purchase/Constants/PurchaseFinanceSource.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Purchase\Constants;
+
+final class PurchaseFinanceSource
+{
+    public const MODULE = 'purchase';
+
+    public const GOODS_RECEIPT = 'goods_receipt_note';
+
+    public const GOODS_RECEIPT_LINE = 'goods_receipt_note_line';
+}
