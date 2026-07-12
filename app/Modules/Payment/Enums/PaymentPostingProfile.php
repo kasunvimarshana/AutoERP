513818dfb98817f1c170599b9e2bb5fbd729e2e6
kasun vimarshana <kasunvmail@gmail.com>
2026-07-12 14:49:25 +0530

    Add semantic payment posting profiles

---
+++ app/Modules/Payment/Enums/PaymentPostingProfile.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Payment\Enums;
+
+enum PaymentPostingProfile: string
+{
+    case CustomerSettlement = 'customer_receipt';
+    case SupplierSettlement = 'supplier_payment';
+    case CustomerAdvance = 'customer_advance';
+    case SupplierAdvance = 'supplier_advance';
+    case RentalDeposit = 'rental_deposit';
+}
