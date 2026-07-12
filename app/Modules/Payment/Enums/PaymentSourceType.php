04a3e82d29a894874630ab853ddd52e372409c9c
kasun vimarshana <kasunvmail@gmail.com>
2026-07-12 14:49:40 +0530

    Add registered payment source types

---
+++ app/Modules/Payment/Enums/PaymentSourceType.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Payment\Enums;
+
+enum PaymentSourceType: string
+{
+    case RentalDepositRequirement = 'rental_deposit_requirement';
+    case PaymentRefund = 'payment_refund';
+}
