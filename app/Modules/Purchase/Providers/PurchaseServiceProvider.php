db0c2726087736cf7ce3c20189f869a28cc83a8e
kasun vimarshana <kasunvmail@gmail.com>
2026-07-12 22:33:32 +0530

    feat(purchase): register invoice restoration handler

--- app/Modules/Purchase/Providers/PurchaseServiceProvider.php
+++ app/Modules/Purchase/Providers/PurchaseServiceProvider.php
@@ -5,18 +5,18 @@ declare(strict_types=1);
 namespace Modules\Purchase\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Modules\Invoice\Contracts\InvoiceSourceCancellationHandlerInterface;
-use Modules\Purchase\Services\Invoice\PurchaseInvoiceCancellationHandler;
-use Modules\Purchase\Services\PurchaseAuthorizationService;
 use Modules\Core\Contracts\PermissionDefinitionRegistryInterface;
+use Modules\Invoice\Contracts\InvoiceSourceRestorationHandlerInterface;
+use Modules\Purchase\Services\Invoice\PurchaseInvoiceRestorationHandler;
+use Modules\Purchase\Services\PurchaseAuthorizationService;
 
 final class PurchaseServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->app->tag(
-            [PurchaseInvoiceCancellationHandler::class],
-            InvoiceSourceCancellationHandlerInterface::TAG,
+            [PurchaseInvoiceRestorationHandler::class],
+            InvoiceSourceRestorationHandlerInterface::TAG,
         );
     }
 
