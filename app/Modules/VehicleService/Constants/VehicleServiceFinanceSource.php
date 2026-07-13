5d576aee1a826b5e1285a90d4f40a0dfbbfc299d
kasun vimarshana <kasunvmail@gmail.com>
2026-07-13 20:22:19 +0530

    feat(vehicle-service): define inventory Finance source identity

---
+++ app/Modules/VehicleService/Constants/VehicleServiceFinanceSource.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\VehicleService\Constants;
+
+final class VehicleServiceFinanceSource
+{
+    public const MODULE = 'vehicle_service';
+
+    public const INVENTORY_ISSUE = 'vehicle_service_inventory_issue';
+
+    public const JOB_LINE = 'vehicle_service_job_line';
+}
