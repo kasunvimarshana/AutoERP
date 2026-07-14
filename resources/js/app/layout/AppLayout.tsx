bd49598849760008e0494583a5b5d96c8a40f40b
kasun vimarshana <kasunvmail@gmail.com>
2026-07-14 15:40:15 +0530

    render resolved tenant navigation

--- resources/js/app/layout/AppLayout.tsx
+++ resources/js/app/layout/AppLayout.tsx
@@ -1,11 +1,11 @@
-import { tenantNavigationSections } from '@/app/navigation/navigationConfig';
+import { tenantWorkspaceNavigationSections } from '@/app/navigation/tenantWorkspaceNavigation';
 import { DASHBOARD_PATH } from '@/app/routePaths';
 import { WorkspaceLayout } from './WorkspaceLayout';
 
 export function AppLayout() {
     return (
         <WorkspaceLayout
-            sections={tenantNavigationSections}
+            sections={tenantWorkspaceNavigationSections}
             homePath={DASHBOARD_PATH}
             workspaceLabel="Business workspace"
             mode="tenant"
