a3c6ecccaf4d025cf2fdb286107f3839cf22034b
kasun vimarshana <kasunvmail@gmail.com>
2026-07-14 15:42:24 +0530

    use canonical HR module code

--- resources/js/app/access/hrRouteEntitlements.ts
+++ resources/js/app/access/hrRouteEntitlements.ts
@@ -1,9 +1,12 @@
+import { TENANT_MODULE_CODE } from '@/app/access/tenantModules';
 import { hrPermissions } from '@/modules/hr/hrPermissions';
 import { operational, type EntitlementRule } from './routeEntitlementPolicy';
 
+const HR_MODULES = [TENANT_MODULE_CODE.HR] as const;
+
 export const hrRouteEntitlements: readonly EntitlementRule[] = [
-    operational('/hr/employees/create', ['hr'], [hrPermissions.employeesCreate]),
-    operational('/hr/employees/:id/edit', ['hr'], [hrPermissions.employeesUpdate]),
-    operational('/hr/employees/:id', ['hr'], [hrPermissions.employeesView]),
-    operational('/hr/employees', ['hr'], [hrPermissions.employeesView]),
+    operational('/hr/employees/create', HR_MODULES, [hrPermissions.employeesCreate]),
+    operational('/hr/employees/:id/edit', HR_MODULES, [hrPermissions.employeesUpdate]),
+    operational('/hr/employees/:id', HR_MODULES, [hrPermissions.employeesView]),
+    operational('/hr/employees', HR_MODULES, [hrPermissions.employeesView]),
 ];
