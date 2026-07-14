6c7e3476355a40cd111dcb81e7b9d407219ee5b2
kasun vimarshana <kasunvmail@gmail.com>
2026-07-14 16:41:37 +0530

    allow HR master workspace access

--- resources/js/app/access/hrRouteEntitlements.ts
+++ resources/js/app/access/hrRouteEntitlements.ts
@@ -4,9 +4,15 @@ import { operational, type EntitlementRule } from './routeEntitlementPolicy';
 
 const HR_MODULES = [TENANT_MODULE_CODE.HR] as const;
 
+export const hrWorkspaceViewPermissions = [
+    hrPermissions.employeesView,
+    hrPermissions.masterDataView,
+    hrPermissions.masterDataManage,
+] as const;
+
 export const hrRouteEntitlements: readonly EntitlementRule[] = [
     operational('/hr/employees/create', HR_MODULES, [hrPermissions.employeesCreate]),
     operational('/hr/employees/:id/edit', HR_MODULES, [hrPermissions.employeesUpdate]),
     operational('/hr/employees/:id', HR_MODULES, [hrPermissions.employeesView]),
-    operational('/hr/employees', HR_MODULES, [hrPermissions.employeesView]),
+    operational('/hr/employees', HR_MODULES, hrWorkspaceViewPermissions),
 ];
