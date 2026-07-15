85c2a5c4aa0b47e652c39bd13d468de5d827660e
kasun vimarshana <kasunvmail@gmail.com>
2026-07-15 08:10:13 +0530

    remove labor commission role ownership

--- app/Modules/VehicleService/Models/VehicleServiceLaborItemCommissionRule.php
+++ app/Modules/VehicleService/Models/VehicleServiceLaborItemCommissionRule.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Modules\Core\Models\TenantOwnedModel;
 use Modules\Item\Models\Item;
 use Modules\VehicleService\Enums\VehicleServiceCommissionType;
-use Modules\VehicleService\Enums\VehicleServiceWorkforceRole;
 
 final class VehicleServiceLaborItemCommissionRule extends TenantOwnedModel
 {
@@ -23,7 +22,6 @@ final class VehicleServiceLaborItemCommissionRule extends TenantOwnedModel
             'organization_unit_id' => 'integer',
             'item_id' => 'integer',
             'row_version' => 'integer',
-            'role_type' => VehicleServiceWorkforceRole::class,
             'commission_type' => VehicleServiceCommissionType::class,
             'commission_value' => 'decimal:6',
             'is_active' => 'boolean',
