9ca014efcb786a2e31c3e52f74dda39df2d5f590
kasun vimarshana <kasunvmail@gmail.com>
2026-07-16 11:02:55 +0530

    preserve service job test import order

--- resources/js/modules/vehicle-service/components/VehicleServiceJobForm.test.tsx
+++ resources/js/modules/vehicle-service/components/VehicleServiceJobForm.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import type { NamedResource } from '@/shared/types/common';
 import { TestRouter } from '@/test/TestRouter';
+import type { NamedResource } from '@/shared/types/common';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import type { VehicleServiceJob } from '../vehicleServiceTypes';
 import { VehicleServiceJobForm } from './VehicleServiceJobForm';
