185ea00171176504f7433e60e0c37914c5d44638
kasun vimarshana <kasunvmail@gmail.com>
2026-07-18 21:05:42 +0530

    Remove Vehicle Rental payment source

--- app/Modules/Payment/Enums/PaymentSourceType.php
+++ app/Modules/Payment/Enums/PaymentSourceType.php
@@ -6,7 +6,6 @@ namespace Modules\Payment\Enums;
 
 enum PaymentSourceType: string
 {
-    case RentalDepositRequirement = 'rental_deposit_requirement';
     case PaymentRefund = 'payment_refund';
     case PaymentAllocation = 'payment_allocation';
 }
