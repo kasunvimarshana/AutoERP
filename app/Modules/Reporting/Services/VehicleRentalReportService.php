7b822ba50e80d4c10729663768df03bdcdb4efea
kasun vimarshana <kasunvmail@gmail.com>
2026-07-21 13:25:00 +0530

    Fail explicitly for unknown Vehicle Rental reports

--- app/Modules/Reporting/Services/VehicleRentalReportService.php
+++ app/Modules/Reporting/Services/VehicleRentalReportService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Modules\Reporting\Services;
 
 use Illuminate\Support\Collection;
+use InvalidArgumentException;
 use Modules\Reporting\DTOs\ReportDefinition;
 
 final class VehicleRentalReportService
@@ -40,7 +41,7 @@ final class VehicleRentalReportService
             self::RUNNING_CHART, self::RENTAL_HISTORY => $this->operations->run($key, $params, $this->definition($key)),
             self::CUSTOMER_INVOICES, self::OWNER_VOUCHERS => $this->financial->run($key, $params, $this->definition($key)),
             self::CHART_EXCEPTIONS => $this->exceptions->run($params, $this->definition($key)),
-            default => $this->definitions->get($key)->toArray(),
+            default => throw new InvalidArgumentException("Vehicle Rental report [{$key}] is not defined."),
         };
     }
 
@@ -51,7 +52,7 @@ final class VehicleRentalReportService
             self::RUNNING_CHART, self::RENTAL_HISTORY => $this->operations->exportRows($key, $params),
             self::CUSTOMER_INVOICES, self::OWNER_VOUCHERS => $this->financial->exportRows($key, $params),
             self::CHART_EXCEPTIONS => $this->exceptions->exportRows($params),
-            default => collect(),
+            default => throw new InvalidArgumentException("Vehicle Rental report [{$key}] is not defined."),
         };
     }
 }
